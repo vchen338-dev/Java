41d453cd3541943d18ff7f0fb03bd1d86c9876d2
Jochen-code <vchen338@gmail.com>
2025-08-25 21:16:27 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package practicefunda;
+
+public class HelloWorld {
+
+	
+	public static void main(String[] args) {
+		// System.out.println(New Line); 
+		System.out.println("Hello");
+		System.out.println("World");
+		
+		// System.out.print(Without New Line); 
+		System.out.print("Hello World");
+	}
+
+}
